a1f61259afb6ee74829279befa20fdbd203211e3
shawon1fb <shawon0187@gmail.com>
2018-09-04 22:56:22 +0600

    this is second commit

---
+++ function.py
@@ -0,0 +1,25 @@
+
+def fun(s):
+    s.sort()
+    
+    for i,j in s.items() :
+        
+        print(str(i)+" = "+str(j))
+
+
+
+def main():
+    
+    x=[('aa',10)]
+    
+    for a,b in x:
+        print(a,b)
+    a=      
+    
+    
+    
+    
+    
+    
+if __name__=="__main__":
+	main()
